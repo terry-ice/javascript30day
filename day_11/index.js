6339917ef941a54c19fa7e6ae305618653692a86
terry-ice <326959013@qq.com>
2018-04-26 20:40:40 +0800

    day-11

--- day_11/index.js
+++ day_11/index.js
@@ -6,16 +6,37 @@ const toggle = player.querySelector('.toggle')
 const skipButtons = player.querySelectorAll('[data-skip]')
 const ranges = player.querySelectorAll('.player__slider')
 
-
-
-function togglePlay() {
-  console.log(video.paused,'video.pased')
-  // if(video.paused){
-  //   video.play();
-  // }else{
-  //   video.pause();
-  // }
-  const method = video.paused? 'play':'pause'
-  video[method]();
+function togglePlay () {
+  const method = video.paused ? 'play' : 'pause'
+  video[method]()
 }
-video.addEventListener('click',togglePlay)
+function updateButton () {
+  toggle.textContent = this.paused ? '►' : '❚❚'
+}
+function skip () {
+  video.currentTime += parseFloat(this.dataset.skip)
+}
+function handleEangeUpdate (e) {
+  // volume 播放的音量
+  // playbackRate  播放时的速率
+  video[this.name] = this.value
+}
+function handleProgress () {
+  const percent = (video.currentTime / video.duration) * 100
+  progressBar.style.flexBasis = `${percent}%`
+}
+function scrub (e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration
+  video.currentTime = scrubTime
+}
+video.addEventListener('click', togglePlay)
+video.addEventListener('play', updateButton)
+video.addEventListener('pause', updateButton)
+video.addEventListener('timeupdate', handleProgress)
+
+toggle.addEventListener('click', togglePlay)
+skipButtons.forEach(button => button.addEventListener('click', skip))
+ranges.forEach(range => range.addEventListener('change', handleEangeUpdate))
+ranges.forEach(range => range.addEventListener('mousemove', handleEangeUpdate))
+
+progress.addEventListener('click', scrub)
